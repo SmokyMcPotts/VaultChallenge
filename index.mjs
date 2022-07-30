ba39214fd7c2b6a40b57bc7794a5408e06a74661
SmokyMcPotts <108499060+SmokyMcPotts@users.noreply.github.com>
2022-07-30 11:12:34 -0400

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,57 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+const startingBalance = stdlib.parseCurrency(100);
+
+const accBob = await stdlib.newTestAccount(startingBalance);
+const accAlice = await stdlib.newTestAccount(stdlib.parseCurrency(10000));
+
+console.log('Hello, Alice and Bob!');
+
+console.log('Launching...');
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+
+const choiceArray = [`I'm not here`, `I'm still here`];
+
+const getBalance = async (who) => stdlib.formatCurrency(await stdlib.balanceOf(who));
+
+console.log(`Alice's account balance before is: ${await getBalance(accAlice)}.`);
+console.log(`Bob's account balance before is: ${await getBalance(accBob)}.`);
+
+const shared = () => ({
+  showCountdown: (time) => {
+    //parseInt
+    console.log(parseInt(time));
+  },
+});
+
+console.log('Starting backends...');
+await Promise.all([
+  backend.Alice(ctcAlice, {
+    ...stdlib.hasRandom,
+    ...shared(),
+    inherit: stdlib.parseCurrency(8000),
+    getChoice: () => {
+      const choice = Math.floor(Math.random() * 2);
+      console.log(`Alice's choice is '${choiceArray[choice]}'.`);
+      return (choice == 0 ? false : true);
+    }
+    // implement Alice's interact object here
+  }),
+  backend.Bob(ctcBob, {
+    ...stdlib.hasRandom,
+    ...shared(),
+    acceptTerms: (value) => {
+      console.log(`Bob accepts the terms of the contract for ${stdlib.formatCurrency(value)}.`);
+      return true;
+    },
+    // implement Bob's interact object here
+  }),
+]);
+
+console.log(`Alice's account balance after is: ${await getBalance(accAlice)}.`);
+console.log(`Bob's account balance after is: ${await getBalance(accBob)}.`);
+
+console.log('Goodbye, Alice and Bob!');
